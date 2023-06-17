4761119b617d8be419128679a4ebf875aa28e256
Amit Kumar <117463564+Amit8127@users.noreply.github.com>
2023-06-17 14:19:35 +0530

    implements user and parkingLot controllers class

---
+++ src/main/java/com/driver/model/Payment.java
@@ -0,0 +1,54 @@
+package com.driver.model;
+
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "PAYMENTS")
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private Boolean paymentCompleted;
+
+    private PaymentMode paymentMode;
+
+    @OneToOne
+    @JoinColumn
+    private Reservation reservation;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Boolean getPaymentCompleted() {
+        return paymentCompleted;
+    }
+
+    public void setPaymentCompleted(Boolean paymentCompleted) {
+        this.paymentCompleted = paymentCompleted;
+    }
+
+    public PaymentMode getPaymentMode() {
+        return paymentMode;
+    }
+
+    public void setPaymentMode(PaymentMode paymentMode) {
+        this.paymentMode = paymentMode;
+    }
+
+    public Reservation getReservation() {
+        return reservation;
+    }
+
+    public void setReservation(Reservation reservation) {
+        this.reservation = reservation;
+    }
+}
