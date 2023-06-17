9fa649cadb664fce7ed5a367b4fea32b99f9792a
Amit Kumar <117463564+Amit8127@users.noreply.github.com>
2023-06-17 19:18:41 +0530

    Change in addSpot method

--- src/main/java/com/driver/services/impl/ParkingLotServiceImpl.java
+++ src/main/java/com/driver/services/impl/ParkingLotServiceImpl.java
@@ -29,12 +29,12 @@ public class ParkingLotServiceImpl implements ParkingLotService {
     public Spot addSpot(int parkingLotId, Integer numberOfWheels, Integer pricePerHour) {
         Spot spot = new Spot();
         spot.setPricePerHour(pricePerHour);
-        if(numberOfWheels <= 2) {
-            spot.setSpotType(SpotType.TWO_WHEELER);
-        } else if(numberOfWheels > 2 && numberOfWheels <= 4) {
+        if(numberOfWheels > 2 && numberOfWheels <= 4){
             spot.setSpotType(SpotType.FOUR_WHEELER);
-        } else {
+        }else if(numberOfWheels > 4){
             spot.setSpotType(SpotType.OTHERS);
+        }else{
+            spot.setSpotType(SpotType.TWO_WHEELER);
         }
 
         ParkingLot parkingLot = parkingLotRepository1.findById(parkingLotId).get();
